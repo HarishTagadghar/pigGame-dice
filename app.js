2cd30715cb306bd476fab1c83dd2fa3626197738
harish tagadghar <harishtgtg@gmail.com>
2020-03-15 00:05:29 -0700

    changes done with the dice images and current score

---
+++ app.js
@@ -0,0 +1,33 @@
+let score , roundScore , active ;
+score = [0,0];
+roundScore = 0;
+active = 0;
+
+document.querySelector('#score-0').innerHTML = 0;
+document.querySelector('#score-1').innerHTML = 0;
+document.querySelector('#current-0').innerHTML = 0;
+document.querySelector('#current-1').innerHTML = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+    
+document.querySelector('.dice').style.display = 'block';
+
+let dice = Math.floor((Math.random() * 6) + 1) ;
+document.querySelector('.dice').src = `dice-${dice}.png`
+
+if(dice !== 1){
+    roundScore += dice;
+    document.querySelector('#current-'+ active).innerHTML = roundScore;
+ }else{
+     active === 0 ? active = 1 : active = 0 ;
+    roundScore = 0
+    document.querySelector('#current-0').innerHTML = 0;
+document.querySelector('#current-1').innerHTML = 0;
+}
+
+
+
+
+})
